fbefceae9898e6e1508f2f2f6f67b7ae2661cec6
Wijit Siripun <wijit.siripun@gmail.com>
2018-09-24 19:05:43 +0700

    Finished Login (create new user, login logout)     TESTING:  localhost:591/pattanasin/logindemo.html      TODO: Update Password and Delete User

--- pattanasin/src/main/java/com/erp/pattanasin/common/errormsg/ErrorMessage.java
+++ pattanasin/src/main/java/com/erp/pattanasin/common/errormsg/ErrorMessage.java
@@ -3,5 +3,12 @@ package com.erp.pattanasin.common.errormsg;
 public class ErrorMessage {
 	public static final String ADD_USER_SUCCESS = "Add New User Success";
 	public static final String USER_EXIST = "User already exist.  Cannot add";
-
+	public static final String EMPTY_ID_PASSWORD = "Login ID and Password Cannot be empty";
+	public static final String NO_LOGINID = "Login ID does not exist";
+	public static final String INCORRECT_PASSWORD = "The password is incorrect";
+	public static final String LOGIN_SUCCESS = "Login Success";
+	public static final String ALREADY_LOGIN = "User already login";
+	
+	public static final String LOGOUT_FAIL_NOTLOGIN = "User does not login. Invalid to logout";
+	public static final String LOGOUT_SUCCESS = "Logout success";
 }
--- pattanasin/src/main/java/com/erp/pattanasin/server/LoginServlet.java
+++ pattanasin/src/main/java/com/erp/pattanasin/server/LoginServlet.java
@@ -1,9 +1,22 @@
+// 9/24/18:  STATUS:  Finished these functionalities (end-to-end) browser to db
+//	A: Create new user
+//  B: login
+//	C: logout
+
+//	TESTING:	http://localhost:591/pattanasin/logindemo.html
+
+//	TODO:   D: Change password
+//			E: Delete user
+
+
+
 package com.erp.pattanasin.server;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
@@ -31,6 +44,7 @@ public class LoginServlet {
 	//	            alert("RESULT returnMessage=" + data.returnMessage + "   \nreturnCode=" + data.resultCode);
 	//	      });
 	// CALL EXAMPLE 3:
+	//			// $.ajax() is flexible. Can do either POST, GET, etc
 	//			$.ajax({
 	//			    type: 'POST',
 	//			    url: '/pattanasin/loginmgr/create',
@@ -49,9 +63,15 @@ public class LoginServlet {
     public ReturnResult createNewUser( @Context HttpServletRequest request, @Context HttpServletResponse response) 
     														throws ServletException, IOException {
     	ReturnResult returnResult = new ReturnResult();
+    	
+    	String remoteUser = request.getRemoteUser();
     	String loginID = request.getParameter("loginID");
     	String password = request.getParameter("password");
-    	System.out.println("createNewUser method:" + "   ID=" + loginID + "    PW=" + password);
+    	System.out.println("createNewUser method:" + "   ID=" + loginID + "    PW=" + password + "=   remoteUser=" + remoteUser );
+    	if (loginID.equals("") || password.equals("")) {
+    		returnResult.setup(ErrorCode.FAIL, ErrorMessage.EMPTY_ID_PASSWORD);
+    		return returnResult;
+    	}
     	
     	EmployeeDAO empDAO = new EmployeeDAO();
     	Employee emp = empDAO.getEmployeeInfoWithLoginID(loginID);
@@ -100,85 +120,123 @@ public class LoginServlet {
     		returnResult.setup(ErrorCode.FAIL, ErrorMessage.USER_EXIST);
     	}
     	
+    	// Create a session for the new user. It will also create JSESSIONID
+    	HttpSession session = request.getSession();
+    	
+    	session.setAttribute("userName", loginID);		// remember the loginID
+    	session.setMaxInactiveInterval(60);		// invalidate the session in 60 seconds
+    	
     	return returnResult;
     	
     }
     
 
-    
-    // CALL EXAMPLE:   http://localhost:591/pattanasin/loginmgr/create
     @POST
-    @Path("create")  
-    public void createNewUser1( @Context HttpServletRequest request, @Context HttpServletResponse response) 
-    														throws ServletException, IOException {
-
-    	String loginID = request.getParameter("loginID");
-    	String password = request.getParameter("password");
-    	System.out.println("createNewUser method:" + "   ID=" + loginID + "    PW=" + password);
-    	
-    	EmployeeDAO empDAO = new EmployeeDAO();
-//    	Employee emp = empDAO.getEmployeeInfoWithLoginID(loginID);
-//    	if (emp == null) {
-//    		System.out.println("loginServlet: emp not found xxxxx ");
-//    	} else {
-//    		System.out.println("loginServlet: emp found yyyyy");
-//    	}
-    	
-
-    	String pass = "pass";
-    	char[] passwordChars = pass.toCharArray();
-    	
-    	// saltBytes example:  [52, -51, -97, -57, 26, 116, 40, -110, 22, -29, 86, 12, -22, -13, -85, 38]
-        //byte[] saltBytes = Password.getSalt();		//  16 bytes salt.
-    	
-    	// saltString example: "34cd9fc71a74289216e3560ceaf3ab26"
-        //String saltString = org.jboss.resteasy.util.Hex.encodeHex(saltBytes);
-        String saltString = "34cd9fc71a74289216e3560ceaf3ab26";
-        byte[] saltBytes = org.jboss.resteasy.util.Hex.decodeHex(saltString);
-        System.out.println("saltByte LEN (should be 16)=" + saltBytes.length);   
-        System.out.println("SaltString=" + saltString + "=    SaltString Len (should be 32)=" + saltString.length());
-
-        
-        // Password class is com.erp.pattanasin.common.Password
-        byte[] hashedBytes = Password.hashPassword(passwordChars, saltBytes, PBKDF2_ITERATIONS, KEY_LENGTH);
-        String hashedString = org.jboss.resteasy.util.Hex.encodeHex(hashedBytes);
-        System.out.println("hashedString=" + hashedString);
-        System.out.println("hashedStringLenght=" + hashedString.length() );
-
-
-    	
-    	Employee emp = new Employee();
-    	emp.setLoginID(request.getParameter("loginID"));
-    	
-
-    	
-    }    
-    
-    
-    
-    
-    
-    
-    
-    
-	//http://localhost:591/tipsuwan/loginmgr/tutorial/helloname/abc_444  chrome
-	//http://localhost:591/tipsuwan/loginmgr/tutorial/helloname/abc_444
-	
-	// http://localhost:591/tipsuwan/loginmgr/login
-	// @Consumes(MediaType.APPLICATION_FORM_URLENCODED )
-    @POST
-    @Path("login")  
-    public void login( @Context HttpServletRequest request, @Context HttpServletResponse response) 
+    @Path("login")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ReturnResult login( @Context HttpServletRequest request, @Context HttpServletResponse response) 
     														throws ServletException, IOException {
         // The redirection to "222 also worked in May 2018
     	//response.setHeader("Location", "/tipsuwan/tutorial/helloname/222");
         //response.sendRedirect("/tipsuwan/tutorial/helloname/222");
         
     	System.out.println("IN login method");
-        response.setHeader("Location", "/pattanasin/index.html");
-        response.sendRedirect("/pattanasin/index.html");
+    	
+    	// Check if userID and password are not empty string
+    	ReturnResult returnResult = new ReturnResult();
+    	String loginID = request.getParameter("loginID");
+    	String password = request.getParameter("password");
+    	if (loginID.equals("") || password.equals("")) {
+    		returnResult.setup(ErrorCode.FAIL, ErrorMessage.EMPTY_ID_PASSWORD);
+    		return returnResult;
+    	}
+    	
+    	// Check if loginID is in DB
+    	EmployeeDAO empDAO = new EmployeeDAO();
+    	Employee emp = empDAO.getEmployeeInfoWithLoginID(loginID);
+    	if (emp == null) {
+    		returnResult.setup(ErrorCode.FAIL, ErrorMessage.NO_LOGINID);
+    		return returnResult;
+    	}
+    	
+    	char[] passwordChars = password.toCharArray();
+        String saltString = emp.getSalt();
+        byte[] saltBytes = org.jboss.resteasy.util.Hex.decodeHex(saltString);
+        
+        byte[] passwordBytes = Password.hashPassword(passwordChars, saltBytes, PBKDF2_ITERATIONS, KEY_LENGTH);
+        String passwordString = org.jboss.resteasy.util.Hex.encodeHex(passwordBytes);
+        
+        // Compare Hash password string to the one from DB
+        if (! emp.getPassword().equals(passwordString)) {
+        	// wrong password
+        	returnResult.setup(ErrorCode.FAIL, ErrorMessage.INCORRECT_PASSWORD);
+        	return returnResult;
+        }
+            	
+    	// "false" => Retrieve the session, but do not create a new session if there is none.
+    	HttpSession session = request.getSession(false);
+    	if (session == null) {
+    		System.out.println("TEST 1: THERE IS NO SESSION.  Create one");
+        	session = request.getSession();		// Retrieve the session. Create a new one if not exist.
+        	if (session == null) {
+        		// Should not get here.
+        		System.out.println("TEST 2:  STILL THERE IS NO SESSION");
+        	} else {
+        		System.out.println("TEST 2: JUST CREATE A NEW SESSION");
+            	session.setAttribute("userName", loginID);		// remember the loginID
+            	session.setMaxInactiveInterval(60);		// invalidate the session in 60 seconds
+        	}
+    	} else {
+    		System.out.println("TEST 1: SESSION EXIST.  DO NOTHING");
+    		String sessionID = session.getId();				// This is JSESSIONID cookie
+    		String remoteUserId = request.getRemoteUser();
+    		String wsCreateUserName = (String) session.getAttribute("userName");
+    		String extraMessage = "    \nsessionID=" + sessionID + "   remoteUserId=" + remoteUserId + "   wsCreateUserName=" + wsCreateUserName;		
+    		returnResult.setup(ErrorCode.SUCCESS, ErrorMessage.ALREADY_LOGIN + " loginID=" + loginID + extraMessage);
+    		return returnResult;
+    	}
+    	
+    	String sessionID = session.getId();
+		String remoteUserId = request.getRemoteUser();
+		String wsCreateUserName = (String) session.getAttribute("userName");
+		String extraMessage = "     \nsessionID=" + sessionID + "   remoteUserId=" + remoteUserId + "   wsCreateUserName=" + wsCreateUserName;
+    	
+    	returnResult.setup(ErrorCode.SUCCESS, ErrorMessage.LOGIN_SUCCESS + " loginID=" + loginID + extraMessage);
+    	return returnResult;
+
+    	
+        //response.setHeader("Location", "/pattanasin/index.html");
+        //response.sendRedirect("/pattanasin/index.html");
     	
     }    
+    
+    
+    @POST
+    @Path("logoutABCD")
+    @Produces(MediaType.APPLICATION_JSON)
+    public ReturnResult logout( @Context HttpServletRequest request, @Context HttpServletResponse response) 
+			throws ServletException, IOException {
+    	
+    	ReturnResult returnResult = new ReturnResult();
+    	HttpSession session = request.getSession(false);	// Retrieve a session. Do not create a new one if session not exist
+    	if (session == null) {
+    		returnResult.setup(ErrorCode.FAIL, ErrorMessage.LOGOUT_FAIL_NOTLOGIN);
+    	} else {
+    		String sessionID = session.getId();
+    		String remoteUserId = request.getRemoteUser();
+    		String wsCreateUserName = (String) session.getAttribute("userName");
+    		String extraMessage = "\nsessionID=" + sessionID + "   remoteUserId=" + remoteUserId + "   wsCreateUserName=" + wsCreateUserName;
+    		
+        	session.invalidate();		// user needs to login again after invalidate
+        	
+        	returnResult.setup(ErrorCode.SUCCESS, ErrorMessage.LOGOUT_SUCCESS + extraMessage);
+        	System.out.println("LOGOUT:   Have called invalidate()");   		
+    	}
+
+
+    	return returnResult;
+    }    
+    
     
     
     
@@ -204,60 +262,6 @@ public class LoginServlet {
     	return "sale1:Sale";
     }
     
-    
-    @GET
-    @Path("logout")
-    public String logout( @Context HttpServletRequest request, @Context HttpServletResponse response) 
-			throws ServletException, IOException {
-    	
-    	request.getSession().invalidate();		// user needs to login again after invalidate
-    	return "logoutSuccess";
-    	
-    }
-    
-    
-//	How to get ID and Role on client side    
-//  
-//		$.post("/tipsuwan/loginmgr/newUser",
-//		        function(data,status){
-//		            alert("Data: " + data + "\nStatus: " + status);
-//		         });  
-  @POST
-  @Path("newUser")
-  public String createNewUser(@FormParam("userID") String userID,
-		  					@FormParam("firstName") String firstName,
-		  					@FormParam("lastName") String lastName,
-		  					@FormParam("email") String email,
-		  					@FormParam("phone") String phone,
-		  					@FormParam("role")  String role) 
-	{
-	  String returnMessage = "";
-	  
-  	// Get userName from the login session.
-	  Employee emp = new Employee();
-	  emp.setLoginID(userID);
-	  emp.setFirstName(firstName);
-	  emp.setLastName(lastName);
-	  emp.setEmail(email);
-	  emp.setPhoneNumber(phone);
-	  emp.setPassword("temporary");
-	  // return 
-	  
-	  System.out.println(emp.toString() + " role = " + role);
-	  
-	  
-  	  EmployeeDAO empDAO = new EmployeeDAO();
-  	  if(empDAO.createOrUpdateEmployee(emp) && empDAO.createOrUpdateEmployeeRole(emp, role)) {
-  		  returnMessage = "New User " + userID+ " has been successfully created!";
-  	  } else {
-  		  returnMessage = "ERROR: Unable to create user.";
-  	  }
-  	
-  	  return returnMessage;
-  	
-	}
-    
-
 }
 
 
